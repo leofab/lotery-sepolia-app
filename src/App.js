e692a021c53ad07f213c8085778dc9e89b5954d9
Leo Fabricio <leofabr3@gmail.com>
2023-11-22 20:48:49 -0300

    setting players array and balance and calling it inside component App

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [manager, setManager] = useState('');
+  let [players, setPlayers] = useState([]);
+  let [balance, setBalance] = useState('');
   useEffect(() => {
     const fetchData = async () => {
       try {
         if (web3 && web3.eth && web3.eth.net.isListening()) {
           const managerData = await lotery.methods.getManager().call();
+          let playersData = await lotery.methods.getPlayers().call();
+          let balanceData = await web3.eth.getBalance(lotery.options.address);
+          setBalance(balanceData);
+          setPlayers(playersData);
           setManager(managerData);
         } else {
           console.log('Web3 provider is not available or connected');
@@ -23,7 +29,11 @@ function App() {
   return (
     <div className="App">
       <h2>Lotery Contract</h2>
-      <p>This contract is managed by {manager}</p>
+      <p>
+        This contract is managed by {manager}.
+        There are currently {players.length} people entered,
+        competing to win {web3.utils.fromWei(balance, 'ether')} ether!
+      </p>
     </div>
   );
 }
